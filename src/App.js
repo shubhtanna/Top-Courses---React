d2c80c3957ed391055f2ccbce6b962a796b41f15
shubhtanna <tannashubh109@gmail.com>
2024-03-25 23:20:34 +0530

    React Course Catalog Web

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import { apiUrl , filterData } from "./data";
+import Filter from "./Components/Filter";
+import Cards from "./Components/Cards";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import Spinner from "./Components/Spinner";
+
+const App = () => {
+
+  const[courses, setCourses] = useState([]);
+  const [laoding, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setCourses(data.data);
+
+      } catch (data) {
+        toast.error("Something Went Wrong");
+      }
+      setLoading(false);
+    };
+    useEffect(() => {
+        fetchData();
+  }, []);
+
+  return(
+    <div  className="min-h-screen flex-col flex bg-bgDark2">
+        <div>
+            <Navbar/>
+        </div>
+      
+        <div className="bg-bgDark2">
+          <div>
+            <Filter
+              filterData = {filterData}
+              category={category}
+            setCategory={setCategory}
+            />
+          </div>
+        
+      
+        <div className="w-11/12 max-w-[1200px] min-h-[50vh] mx-auto flex flex-wrap justify-center items-center">
+        {
+            laoding ? (
+              <Spinner />
+            ) : (
+              <Cards courses={courses} category={category} />
+            )
+          }
+        </div>
+        </div>
+    </div>
+  );
+};
+
+
+export default App;
